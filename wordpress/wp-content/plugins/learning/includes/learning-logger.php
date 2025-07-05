02591472f2c28c66791bac1cee30968bfa454dcc
Paresh Lamichhane <PareshLamichhane@gmail.com>
2025-07-05 19:22:00 +0545

    Day 34

--- wordpress/wp-content/plugins/learning/includes/learning-logger.php
+++ wordpress/wp-content/plugins/learning/includes/learning-logger.php
@@ -19,6 +19,31 @@ class Learning_Logger {
         add_filter( 'manage_post_posts_columns', [ self::class, 'add_learning_column' ] );
         add_action( 'manage_post_posts_custom_column', [ self::class, 'render_learning_column' ], 10, 2 );
         add_action( 'wp_dashboard_setup', [ self::class, 'add_learning_dashboard_widget' ] );
+        add_action('rest_api_init', function () {
+        register_rest_route('learning/v1', '/snippets', [
+            'methods' => 'GET',
+            'callback' => function () {
+                $query = new WP_Query([
+                    'post_type' => 'learning_snippet',
+                    'posts_per_page' => 5,
+                    'post_status' => 'publish',
+                ]);
+                
+                $snippets = [];
+                while ($query->have_posts()) {
+                    $query->the_post();
+                    $snippets[] = [
+                        'title' => get_the_title(),
+                        'excerpt' => get_the_excerpt(),
+                        'source' => get_post_meta(get_the_ID(), '_learning_source', true),
+                    ];
+                }
+                wp_reset_postdata();
+                return $snippets;
+            },
+            'permission_callback' => '__return_true'
+        ]);
+    });
     }
     public static function add_learning_dashboard_widget() {
         wp_add_dashboard_widget(
